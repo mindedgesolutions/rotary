6dcc28747bc20b792a024d016292eb99639f3f27
mindedgesolutions <30463686+mindedgesolutions@users.noreply.github.com>
2017-07-26 18:52:00 +0530

    Delete config-2.php

--- include/config-2.php
+++
@@ -1,8 +0,0 @@
-<?php
-mysql_connect('localhost', 'mindedgeteach2', 'SuHiNa@1979');
-mysql_select_db('rotary32_teach2');
-
-error_reporting(0);
-session_start();
-
-date_default_timezone_set("Asia/Kolkata");
