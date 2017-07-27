f7a96eb25319ab42ec78e624968d139eb571ae05
mindedgesolutions <30463686+mindedgesolutions@users.noreply.github.com>
2017-07-27 10:40:43 +0530

    Add files via upload

---
+++ merchant-3/config.php
@@ -0,0 +1,10 @@
+<?php
+class payment_config{
+	var $Url = "https://payment.atomtech.in/paynetz/epi/fts";
+	var $Login="18143";
+	var $Password="ROTARY@123";
+	var $MerchantName="ATOM";
+	var $TxnCurr="INR";
+	var $TxnScAmt="0";
+}
+?>
